1766fb083e5aa995c4ef0aec2ddfdfee48834cb6
Harits25 <excialfh@gmail.com>
2022-01-19 11:44:32 +0700

    Fundamental_JS

---
+++ FundamentalJS/Fundamental/Komentar.js
@@ -0,0 +1,6 @@
+// Komentar dengan satu baris
+
+/* Komentar yang mendukung lebih dari satu baris 
+    Contohnya :
+    - Salah satunya ini
+*/
---
+++ FundamentalJS/Fundamental/Statmant.js
@@ -0,0 +1,7 @@
+let name = "Kodekami.com"
+console.log(name)
+
+if(true) {
+    console.log("Statment 1")
+    alert("Statment 2")
+};
